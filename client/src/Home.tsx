6afd46525940c842bffa3ab5eac4ab5949553936
salah559 <142224356+salah559@users.noreply.github.com>
2025-09-30 01:02:14 +0100

    Add files via upload

---
+++ client/src/Home.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <ServicesSection />
+        <PortfolioSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
